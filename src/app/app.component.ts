cf5de2ebc6b6be7556cdc68fac7671b65bb927f1
perlasamanez <perlitadelcastillo@gmail.com>
2024-11-07 23:03:02 -0500

    adding bootstrap and putting a temp page under construction, also setting up all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    RouterOutlet, 
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
